a2791fa1f4598a74960db8b8410eea58ca9ee721
bholanath <bhola.mact2002@gmail.com>
2026-03-19 15:59:52 +0530

    Added lamda examples

--- test_function.py
+++ test_function.py
@@ -1,9 +1,34 @@
 
+
 nums=[1,2,3,4,5,6,7,8,9,10]
-print(nums)
+print("nums",nums)
+
+#lamda function
+
+# f= lambda a : a*a
+# result=f(5)
+# print(result)
+#
+# f1= lambda a,b : a+b
+# result=f1(5,6)
+# print(result)
+
+
+
+evens= list(filter(lambda x: x % 2 == 0, nums))
+print("evens",evens)
+odds= list(filter(lambda x: x % 2 != 0, nums))
+print("odds",odds)
+
+
+sqrs= list(map(lambda n:n*n , nums))
+print("sqrs", sqrs)
+
+
+
 
 from functools import reduce
 
 sum=reduce (lambda a,b: a+b, nums)
-print(sum)
+print("sum", sum)
 
